bb4d1be0416d494e27ace14c5698774f529af177
Sajid <Sajed1990@gmail.com>
2015-01-09 00:12:50 +0600

    init

--- laravel_fxo/app/views/layouts/sitepage.blade.php
+++ laravel_fxo/app/views/layouts/sitepage.blade.php
@@ -36,14 +36,12 @@
                             <li class="disabled"><a href="#"><i class="fa fa-envelope"></i> info@freextraoffer.com</a></li>
                             <li class="disabled"><a href="#"><i class="fa fa-phone"></i> (+88) - 01716991580</a></li>
                         </ul>
-                        <ul class="topbar-nav topbar-right hidden-xs">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="https://www.facebook.com/pages/freextraoffercom/382623471905875" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        </ul>
+                        <div class="topbar-nav topbar-right">
+                            <form class="search-form">
+                                <input type="text" name="">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -81,11 +79,44 @@
         <!-- begin:footer -->
         <div id="footer">
             <div class="container">
+                <div class="row">
+                    <div class="col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8">
+                        
+                        <h3>Newsletter Subscription</h3>
+                        <form class="form-inline">
+                            <div class="form-group email">
+                                <input class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group submit-btn">
+                                <button type="submit" class="btn btn-default">Subscribe</button>
+                            </div>
+                        </form>
+                        <ul class="footer-menu">
+                            <li><a>About Us</a></li><li><a>Contact Us</a></li><li><a>Privacy Policy</a></li>
+                            <li><a>Terms and Condition</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- break -->
+
+
                 <!-- begin:copyright -->
+                
                 <div class="row">
                     <div class="col-md-12 copyright">
-                        <p>&copy; 2014 by <strong><a href="http://www.webnit24.com" target="_blank">webnit24</a></strong> All Right Reserved.</p>
-                        <a href="#top" class="btn btn-success scroltop"><i class="fa fa-angle-up"></i></a>
+                        <div class="col-md-6">
+                            <p>&copy; 2014 by <strong><a href="http://www.webnit24.com" target="_blank">webnit24</a></strong> All Right Reserved.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="social-media topbar-nav topbar-right hidden-xs">
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.facebook.com/pages/freextraoffercom/382623471905875" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                            <a href="#top" class="btn btn-success scroltop"><i class="fa fa-angle-up"></i></a>
+                        </div>
                     </div>
                 </div>
                 <!-- end:copyright -->
